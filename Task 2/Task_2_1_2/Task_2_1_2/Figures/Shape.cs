e2a821393e88d7c93dfcbd3b9a2a8e82811c2f25
Pavel Zhulidin <>
2022-01-21 00:28:30 +0400

    Added Interfaces and classes for Figures.

---
+++ Task 2/Task_2_1_2/Task_2_1_2/Figures/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_1_2
+{
+    public abstract class Shape
+    {
+
+
+    }
+}
