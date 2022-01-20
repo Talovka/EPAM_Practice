157ec73f031d2993068e7fdb6336710132fdacfd
Pavel Zhulidin <>
2022-01-20 23:39:40 +0400

    Created files for Task 2.1.2

---
+++ Task 2/Task_2_1_2/Task_2_1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_2_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
