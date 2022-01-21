79123cc8218eaa9537b17e180b8fa3f95d64c120
Pavel Zhulidin <>
2022-01-22 01:08:28 +0400

    Added the properties for Line.

--- Task 2/Task_2_1_2/Task_2_1_2/Figures/Line.cs
+++ Task 2/Task_2_1_2/Task_2_1_2/Figures/Line.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Task_2_1_2
 {
-    class Line
+    class Line: Shape, ILineLengthCalculation
     {
+        private Point _first;
+        private Point _second;
+
+        public double GetLineLength { get { return (int)Math.Sqrt(Math.Pow((_second.X - _first.X), 2) + Math.Pow((_second.Y - _first.Y),2)); } }
+
+        public Line(Point first, Point second) 
+        {
+            _first = first;
+            _second = second;
+        }
+        public override string GetName() => "Линия";
+
     }
 }
