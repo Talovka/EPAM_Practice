d34dd80fc4a879414f2acfa9db07b39341517d54
Pavel Zhulidin <>
2022-01-23 01:14:27 +0400

    Created class User for initializing in program.

---
+++ Task 2/Task_2_1_2/Task_2_1_2/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_1_2
+{
+    class User
+    {
+        private List<Shape> _shape;
+        public string Name { get; set; }
+        public User(string name) 
+        {
+            Name = name;
+            _shape = new List<Shape>();
+        }
+        public void AddShape(Shape shape) 
+        {
+            _shape.Add(shape);
+        }
+    }
+}
