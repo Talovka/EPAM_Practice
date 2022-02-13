9a3d6775facb9917afe35d3fb0e9f685f8663147
Pavel Zhulidin <>
2022-02-13 20:44:53 +0400

    Push first Task in third Block.

---
+++ Task 3/Task_3_1/Task_3_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Serilog;
+
+namespace Task_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerStart();
+            WeakedLinkStart weakedLink = new WeakedLinkStart();
+            RunText text = new RunText();
+        }
+
+        private static void LoggerStart(bool fileLogs = false, string fileName = "logs/example.txt")
+        {
+            if (fileLogs)
+                Log.Logger = new LoggerConfiguration()
+                            .MinimumLevel.Debug()
+                            .WriteTo.Console()
+                            .WriteTo.File(fileName, rollingInterval: RollingInterval.Day)
+                            .CreateLogger();
+            else
+                Log.Logger = new LoggerConfiguration()
+                            .MinimumLevel.Debug()
+                            .WriteTo.Console()
+                            .CreateLogger();
+        }
+    }
+}
---
+++ Task 3/Task_3_1/Task_3_1/WeakedLink.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_1
+{
+    class WeakedLink
+    {
+		private List<int> list;
+		private const int ConstWeaked = 2;
+
+		public int Count { get; init; }
+		public int WeakedNumber { get; init; }
+		public WeakedLink(int count)
+		{
+			Count = count;
+			WeakedNumber = ConstWeaked;
+			list = new List<int>(Count);
+			FillList();
+		}
+		public WeakedLink(int count, int weaked_number)
+		{
+			Count = count;
+			WeakedNumber = weaked_number;
+			list = new List<int>(Count);
+			FillList();
+		}
+
+		private void FillList()
+		{
+			for (int i = 1; i < list.Capacity + 1; i++)
+			{
+				this.list.Add(i);
+			}
+		}
+		public void Start()
+		{
+			for (int i = 1; ; i++)
+			{
+				if (list.Count >= WeakedNumber)
+				{
+					list.RemoveAt(WeakedNumber - 1);
+					list.Sort();
+					Console.WriteLine($"Раунд {i}. Вычеркнут человек. Людей осталось {list.Count}");
+				}
+				else break;
+			}
+			Console.WriteLine("Игра окончена, невозможно вычеркнуть больше людей");
+		}
+	}
+}
